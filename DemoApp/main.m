2a74cde1229ef0194a75363a85d4e0e5f1502342
Trieu Khang <hoangtrieukhang@gmnail.com>
2014-03-28 12:45:42 +0800

    Initial Commit

---
+++ DemoApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DemoApp
+//
+//  Created by Triệu Khang on 28/3/14.
+//  Copyright (c) 2014 Triệu Khang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TKAppDelegate class]));
+    }
+}
